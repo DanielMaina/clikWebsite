62d1345800d6cd81a09e0d54007cf0d099e05773
DanielMaina <daniel_maina9@hotmail.com>
2020-11-02 01:12:27 -0500

    init commit

---
+++ src/pages/Landing/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Manage Appointments',
+    headline: 'Leading Scheduling Software across Multiple Industries',
+    description:
+      'We help business owners increase their revenue. Our team of specialist can help you achieve your business goals.',
+    buttonLabel: 'Download',
+    imgStart: '',
+    img: require('../../images/mainLanding.svg'),
+    alt: 'Application',
+    start: ''
+  };
+  
+  export const homeObjTwo = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: "Save Time",
+    headline: 'Extremely quick download process',
+    description:
+      "Instant bookings at the palm of your hand.",
+    buttonLabel: 'Learn More',
+    imgStart: '',
+    img: require('../../images/bookedLanding.svg'),
+    alt: 'Book',
+    start: ''
+  };
+  
+  export const homeObjThree = {
+    primary: false,
+    lightBg: true,
+    lightTopLine: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Instant Setup',
+    headline:
+      'Extremely quick onboarding process',
+    description:
+      "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+    buttonLabel: "Let's chat",
+    imgStart: 'start',
+    img: require('../../images/setupLanding.svg'),
+    alt: 'Setup',
+    start: 'true'
+  };
---
+++ src/pages/Landing/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { homeObjOne, homeObjTwo, homeObjThree } from './Data';
+import { InfoSection } from '../../components';
+
+function Home() {
+  return (
+    <>
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Home;
